0ed3aab1144c398898fa62a66f208e36f25dacf1
orlando1979 <orlando1979@outlook.com>
2020-11-21 20:54:16 -0500

    Inicio curso basico de Angular Con Bezael y creo los componentes principlae con sus rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/products/product.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { DetailsProductComponent } from './components/products/details-product/details-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HomePageComponent,
+    Page404Component,
+    DetailsProductComponent,
+    ProductListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
